c5f35a970882cc54e979a37cba4e1fa694c115d5
Elnur <elnur.rahimov@expressbank.az>
2024-12-05 01:11:09 +0400

    Added project structure

---
+++ src/test/java/az/unibank/msauth/MsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package az.unibank.msauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
